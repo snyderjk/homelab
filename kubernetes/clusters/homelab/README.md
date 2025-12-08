5ff7859bd2851ee95fc8e6f528fef33927f70c26
Jason Snyder <snyderjk@gmail.com>
2025-12-08 07:16:43 -0600

    Significant restructuring of homelab repo, everythig to ansible, flux setup

---
+++ kubernetes/clusters/homelab/README.md
@@ -0,0 +1,5 @@
+# Homelab Cluster
+
+This directory contains Flux configuration for the homelab Kubernetes cluster.
+
+Flux will automatically populate this directory during bootstrap.
--- scripts/decrypt-secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage: ./decrypt-secret.sh path/to/secret.enc.yaml
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <secret-file.enc.yaml>"
-  exit 1
-fi
-
-sops --decrypt "$1"
